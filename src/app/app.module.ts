bd58728902ce323104adacc687b72f5d8178690d
macaskar <askar.satabaldiyev@gmail.com>
2018-05-04 16:48:43 +0600

    Lecture 43: Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { CoursesComponent } from './courses.component';
 import { CourseComponent } from './course/course.component';
 import { CoursesService } from './courses.service';
 
