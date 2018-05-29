4fece20a58e5d024b2d800e51072102509919dd3
macaskar <askar.satabaldiyev@gmail.com>
2018-05-30 00:21:05 +0600

    capitalize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CourseComponent } from './course/course.component';
 import { CoursesService } from './courses.service';
+import { FavouriteComponent } from './favourite/favourite.component';
+import { TitleCasePipe } from './title-case.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent, 
     CourseComponent, 
-    SummaryPipe
+    SummaryPipe, FavouriteComponent, TitleCasePipe
   ],
   imports: [
     BrowserModule, 
---
+++ src/app/favourite/favourite.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'favourite',
+  templateUrl: './favourite.component.html',
+  styleUrls: ['./favourite.component.css']
+})
+export class FavouriteComponent implements OnInit {
+  isFavourite: boolean;
+  
+  constructor() { }
+
+  onClick(){
+    this.isFavourite = !this.isFavourite;
+  }
+
+  ngOnInit() {
+  }
+
+}
